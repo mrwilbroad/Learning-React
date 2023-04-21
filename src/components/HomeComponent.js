b8c0e25e200b6ec89601047ebad121efffd9a4a3
wilbroad Mark <wilbroadmark80@gmail.com>
2023-04-21 09:38:22 +0300

    First section

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Container } from 'react-bootstrap';
+
+class HomeComponent extends Component {
+
+  render() {
+
+    return (
+      <Container className='m-0 p-0' fluid>
+            
+      </Container>
+    )
+  }
+}
+
+export default HomeComponent
--- src/index.js
+++ src/index.js
@@ -4,10 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Routing
+import { BrowserRouter} from 'react-router-dom'
+
+// Bootstrap js
+import 'bootstrap/dist/css/bootstrap.css';
+
+// swipper css
+import 'swiper/swiper.css'
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
